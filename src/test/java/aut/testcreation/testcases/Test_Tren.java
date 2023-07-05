211e946cf00c39596130b5f2f84479554bc1a7b5
Marcelo Magallanes <marcelo.magallanes@tsoftglobal.com>
2023-07-05 11:14:24 -0300

    agrego la funcion captcha en tes_trenes

--- src/test/java/aut/testcreation/testcases/Test_Tren.java
+++ src/test/java/aut/testcreation/testcases/Test_Tren.java
@@ -19,16 +19,26 @@ public class Test_Tren extends SeleniumTestBase {
         homePageTrenes = new HomePageTrenes(DriverFactory.getDriver());
         fechasDisponibles = new FechasDisponibles(homePageTrenes.getDriver());
         homePageTrenes.navegarAlSitio();
+        homePageTrenes.validarCaptcha();
         homePageTrenes.irATrenes();
     }
 
 
     @Test
-    public void TC007_reserva_tren_idaYVuelta_fechasDisponibles(){
+    public void TC007_reserva_tren_idaYVuelta_ingresoFechas(){
         homePageTrenes.irATrenes();
         fechasDisponibles.llenarCamposVacios("Madrid", "Barcelona", "10", "15");
 
     }
+
+    @Test
+    public void TC008_reserva_tren_seleccionFecha(){
+        homePageTrenes.irATrenes();
+        fechasDisponibles.llenarCamposVacios_clickFechas("Almeria", "Ciudad Real");
+
+
+    }
+
     @AfterEach
     public void cerrandoTest(){
         homePageTrenes.getDriver().close();
